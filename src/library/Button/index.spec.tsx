6358cf3c38f54e7d2557a92c4b1250bf7679f8f9
Martin Pastore <martinpablopastore@gmail.com>
2020-04-19 12:23:30 -0300

    feat(Button): add test

--- src/library/Button/index.spec.tsx
+++ src/library/Button/index.spec.tsx
@@ -1,7 +1,58 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Button from "./index";
+import "jest-styled-components";
 
 test("should be defined", () => {
   expect(Button).toBeDefined();
+});
+
+test("should render text and primary button", () => {
+  const { getByText } = render(
+    <Button
+      primary
+      color="#fff"
+      text="Click me"
+      onAction={() => console.log("")}
+    />
+  );
+
+  const displayValue = getByText(/Click me/i);
+  expect(displayValue).toBeInTheDocument();
+  expect(displayValue).toHaveStyleRule("border", "2px solid #56BEB4");
+});
+
+test("should render text and secondary button", () => {
+  const { getByText } = render(
+    <Button
+      secondary
+      color="#fff"
+      text="Click me"
+      onAction={() => console.log("")}
+    />
+  );
+
+  const displayValue = getByText(/Click me/i);
+  expect(displayValue).toBeInTheDocument();
+  expect(displayValue).toHaveStyleRule("border", "2px solid #000");
+});
+
+test("should execute action on click", () => {
+  const logSpy = spyOn(console, "log");
+
+  const { getByText } = render(
+    <Button
+      secondary
+      color="#fff"
+      text="Click me"
+      onAction={() => console.log("test")}
+    />
+  );
+
+  const element = getByText(/Click me/i);
+
+  fireEvent.click(element);
+
+  expect(logSpy).toHaveBeenCalledTimes(1);
+  expect(logSpy).toHaveBeenCalledWith("test");
 });
